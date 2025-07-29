906cabd207243f580ab56e539e6b0bce36bacc7a
Vineet <vineet9949@gmail.com>
2025-07-30 01:31:35 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,10 +1,11 @@
 
-# model.py
+# src/model.py
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import joblib  # Import joblib to save the model
 
 # Load the processed data
 data = pd.read_csv('../data/processed_data.csv')
@@ -24,3 +25,6 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Model Accuracy: {accuracy:.2f}')
+
+# Save the trained model
+joblib.dump(model, './model.pkl')  # Save the model in the src directory
